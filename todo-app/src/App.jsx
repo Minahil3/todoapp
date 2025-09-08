98bd522e2f34b98c646081a3c6f34ed5ed4ccb91
Minahil3 <mnaseer@finalpos.com>
2025-09-08 12:15:49 -0230

    used useEffect hook to save todos in localStorage

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,39 +2,51 @@ import { Header } from "./components/Header"
 import { Tabs } from "./components/Tabs"
 import { TodoInput } from "./components/ToDoInput"
 import { TodoList } from "./components/ToDoList"
-import { useState, createContext, useContext } from "react"
-
+import { useState, createContext, useEffect } from "react"
 
 // Creating an empty container 
 export const TodoContext = createContext();
 
 function App() {
-  const [todos, setTodos] = useState([
-    { input: "Hello! Do a React Course!", complete: true }
-  ]);
+
+
+  // lazy initialization of state here, if nothign is saved in local storage simple use the default value. 
+  // allows to refresh and maintain data by parsing 
+  const [todos, setTodos] = useState(() => {
+  const saved = localStorage.getItem("todos");
+  return saved
+    ? JSON.parse(saved) 
+    : [{ input: "Hello! Do a React Course!", complete: true }];
+});
+
+
   const [selectedTab, setSelectedTab] = useState("Open");
 
-  // Handlers moved inside so we can expose them in context
+  // this ensures that whatever is shown on the ui is also saved. So, this works after rendering
+  // allows us to update storage only when todo changes, otherwise it would store to localstorage everytime it renders and that is essentially waste of space.
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  
   function handleAddTodo(newTodo) {
     const newTodoList = [...todos, { input: newTodo, complete: false }];
     setTodos(newTodoList);
   }
 
   function handleCompleteTodo(index) {
-    let newTodoList = [...todos];
-    let completedTodo = { ...todos[index] };
-    completedTodo.complete = true;
-    newTodoList[index] = completedTodo;
+    const newTodoList = [...todos];
+    newTodoList[index] = { ...todos[index], complete: true };
     setTodos(newTodoList);
   }
 
   function handleDeleteTodo(index) {
-    let newTodoList = todos.filter((_, valIndex) => valIndex !== index);
+    const newTodoList = todos.filter((_, valIndex) => valIndex !== index);
     setTodos(newTodoList);
   }
 
-  //esentially wrapping the app with context so that all components can use it.
   return (
+    // provides all the components with these values, so these are not global variables.
     <TodoContext.Provider
       value={{
         todos,
